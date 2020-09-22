80a01412bd1193b7345e2d5674ee1c95bc08825d
dicaearchus <b.altena@uu.nl>
2020-09-22 14:19:32 +0200

    integrated transformation from polygon masks to writing text file with coordinates of caster and casted

--- eratosthenes/generic/handler_im.py
+++ eratosthenes/generic/handler_im.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 
-def get_image_subset(img, rmin, rmax, cmin, cmax):  # generic
+def get_image_subset(img, bbox):  # generic
     """
     get subset of an image specified by the bounding box extents
     input:   img            array (n x m)     band with boolean values
-             rmin           integer           minimum row
-             rmax           integer           maximum row
-             cmin           integer           minimum collumn
-             cmax           integer           maximum collumn
-    output:  sub_img        array (k x l)     band with boolean values         
+             bbox           array (1 x 4)     array giving:
+                                              [
+                                                  minimum row,
+                                                  maximum row,
+                                                  minimum collumn,
+                                                  maximum collumn
+                                              ]
+    output:  sub_img        array (k x l)     band with values         
     """
-    
-    sub_img = img[rmin:rmax,cmin:cmax]
+    sub_img = img[bbox[0]:bbox[1],bbox[2]:bbox[3]]
     return sub_img
