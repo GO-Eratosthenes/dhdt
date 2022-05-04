af35cdfc10a370ad03986b46d0bcb3769d56fec3
Bas Altena <b.altena@uu.nl>
2022-05-04 09:43:33 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -104,7 +104,7 @@ html_context = {
         '_static/theme_overrides.css',  # override wide tables in RTD theme
         ],
      }
-html_logo = "_static/dhdt-white.png"
+html_logo = "dhdt-white.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
