0c4721dc7b28e0cce4367e5d646208f8a60d60bf
Francesco Nattino <f.nattino@esciencecenter.nl>
2023-02-27 11:40:01 +0100

    include download feature

--- scripts/sentinel-2-imagery/sentinel-2-STAC.py
+++ scripts/sentinel-2-imagery/sentinel-2-STAC.py
@@ -1,11 +1,15 @@
 """
 Generate a SpatioTemporal Asset Catalog (STAC) from the Sentinel-2 metadata
 returned by the Copernicus Open Access Hub API. Assets are linked to files on
-the Sentinel-2 public dataset on Google Cloud Storage (GCS):
+the Sentinel-2 public dataset on Google Cloud Storage (GCS)[1]. Optionally
+download (a subset of) the assets and store these alongside the respective
+scenes' metadata.
 
 https://cloud.google.com/storage/docs/public-datasets/sentinel-2
 
 """
+__version__ = "0.1"
+
 import argparse
 import os
 
@@ -14,6 +18,8 @@ from typing import Dict, Tuple
 import geopandas as gpd
 import pystac
 
+from pystac.utils import make_relative_href
+from stactools.core.copy import move_asset_file_to_item
 from stactools.core.io.xml import XmlElement
 from stactools.sentinel2.stac import create_item
 from stactools.sentinel2.constants import GRANULE_METADATA_ASSET_KEY
@@ -23,27 +29,41 @@ GCS_ROOT_URL = "https://storage.googleapis.com"
 SENTINEL2_BUCKET = "gcp-public-data-sentinel-2"
 
 
-def parse_args():
+def parse_args(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "geojson", type=str, help="Path to the Sentinel-2 metadata file"
-    )
-    parser.add_argument(
-        "--path",
+        "--catalog-path",
         type=str,
         help="Path to the STAC catalog",
-        default="./catalog",
+        required=True,
     )
-    parser.add_argument(
+    subparsers = parser.add_subparsers(required=True)
+
+    create_parser = subparsers.add_parser("create")
+    create_parser.add_argument(
+        "--from-geojson",
+        type=str,
+        help="Path to the Sentinel-2 metadata file",
+        required=True,
+    )
+    create_parser.add_argument(
         "--description",
         type=str,
         help="Brief catalog description",
         default="My STAC catalog",
+        required=True,
     )
-    parser.add_argument(
+    create_parser.add_argument(
         "--template", type=str, help="Catalog structure",
     )
-    return parser.parse_args()
+    create_parser.set_defaults(run=create_stac_catalog)
+
+    download_parser = subparsers.add_parser("download")
+    download_parser.add_argument(
+        "--assets", nargs='*', help="List of asset keys"
+    )
+    download_parser.set_defaults(run=download_assets)
+    return parser.parse_args(args)
 
 
 def _get_granule_href(safe_filename: str) -> str:
@@ -75,7 +95,7 @@ def _get_sensor_array_key_asset_pair(href: str) -> Tuple[str, pystac.Asset]:
         pystac.MediaType.XML if ext == ".gml" else pystac.MediaType.JPEG2000
     )
     asset = pystac.Asset(href=href, media_type=media_type, roles=["metadata"])
-    return f"sensor-metadata-{band_key}", asset
+    return f"sensor_metadata_{band_key}", asset
 
 
 def _create_sensor_array_assets(
@@ -110,37 +130,23 @@ def _create_item(safe_filename: str) -> pystac.Item:
     return item
 
 
-def create_stac_catalog(
-        geojson_path: str, catalog_path: str, catalog_description: str,
-        template: str = None
-) -> None:
+def create_stac_catalog(args: argparse.Namespace) -> None:
     """
     Create a STAC catalog from the scenes' metadata retrieved from the
     Copernicus Open Access Hub API and saved locally in GeoJSON format. Assets
     are linked to the corresponding files on the Sentinel-2 public dataset on
     Google Cloud Storage.
-
-    Parameters
-    ----------
-    geojson_path: str
-        path to the GeoJSON file containing the scenes' metadata
-    catalog_path: str
-        path where to save the STAC catalog
-    catalog_description: str
-        short description of the catalog
-    template: str, optional
-        if provided, organize items in a nested catalog structure according to
-        this template
-    Returns
-    -------
-    pystac.Catalog : list of items corresponding to the Sentinel-2 scenes.
     """
+    catalog_path = args.catalog_path
+    geojson_path = args.from_geojson
+    catalog_description = args.description
+    template = args.template
 
     _, catalog_id = os.path.split(catalog_path)
     catalog = pystac.Catalog(id=catalog_id, description=catalog_description)
 
     scenes_df = gpd.read_file(geojson_path)
-    items = (_create_item(scene.filename) for _, scene in scenes_df.iterrows())
+    items = (_create_item(scene.filename) for _, scene in scenes_df.loc[:2].iterrows())
     catalog.add_items(items)
 
     if template is not None:
@@ -152,14 +158,36 @@ def create_stac_catalog(
     )
 
 
-def main():
-    args = parse_args()
-    create_stac_catalog(
-        geojson_path=args.geojson,
-        catalog_path=args.path,
-        catalog_description=args.description,
-        template=args.template,
-    )
+def download_assets(args: argparse.Namespace) -> None:
+    """
+    Download (a subsection of) the catalog assets and store these alongside the
+    corresponding items.
+    """
+    catalog_path = args.catalog_path
+    asset_keys = args.assets
+
+    path = catalog_path \
+        if catalog_path.endswith("catalog.json") \
+        else f"{catalog_path}/catalog.json"
+    catalog = pystac.Catalog.from_file(path)
+
+    for item in catalog.get_all_items():
+        keys = asset_keys if asset_keys is not None else item.assets.keys()
+        for key in keys:
+            asset = item.assets[key]
+            asset_abs_href = asset.get_absolute_href()
+            asset_href = move_asset_file_to_item(
+                item,
+                asset_abs_href,
+                copy=True,
+            )
+            asset.href = make_relative_href(asset_href, item.get_self_href())
+        item.save_object()
+
+
+def main(args: list = None) -> None:
+    args_parsed = parse_args(args)
+    args_parsed.run(args_parsed)
 
 
 if __name__ == "__main__":
