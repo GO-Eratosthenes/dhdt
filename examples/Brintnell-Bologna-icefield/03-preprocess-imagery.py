740a138ce4d113fae6e27896baa286f782a498df
Bas Altena <b.altena@uu.nl>
2023-03-30 09:38:11 +0200

    Update 03-preprocess-imagery.py
    added description of content

---
+++ examples/Brintnell-Bologna-icefield/03-preprocess-imagery.py
@@ -0,0 +1,5 @@
+"""
+preprocessing of the multi-spectral imagery, doing the following:
+- co-register the imagery to common frame
+- emphasis the illumination component, so shadows are enhanced
+"""
