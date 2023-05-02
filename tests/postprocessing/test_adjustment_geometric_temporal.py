9237ab0ec733f1d6b6101d1cf4e0bcc2d94aa6d2
Bas Altena <b.altena@uu.nl>
2023-05-02 11:46:31 +0200

    Update test_adjustment_geometric_temporal.py

--- tests/postprocessing/test_adjustment_geometric_temporal.py
+++ tests/postprocessing/test_adjustment_geometric_temporal.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-from dhdt.postprocessing.adjustment_geometric_temporal import rotate_disp_field
+from dhdt.generic.attitude_tools import rot_mat
+from dhdt.postprocessing.adjustment_geometric_temporal import \
+    rotate_disp_field, rot_covar, rotate_variance
 
 def test_rotate_disp_field(m=40,n=30):
     # initialization
@@ -17,3 +19,19 @@ def test_rotate_disp_field(m=40,n=30):
     # backward rotation
     UV_bck = rotate_disp_field(UV_frw, -θ)
     assert np.isclose(0, np.max(np.abs(UV - UV_bck)))
+
+def test_rot_covar():
+    # initialization
+    I = np.eye(2)
+    θ = np.random.uniform(low=0., high=360., size=(1,))[0]
+
+    # forward rotation
+    I_frw = rot_covar(I, rot_mat(θ))
+
+    # whole circle rotation
+    I_whl = rot_covar(I_frw, rot_mat(360-θ))
+    assert np.isclose(0, np.max(np.abs(I - I_whl)))
+
+    # backward rotation
+    I_bck = rot_covar(I_frw, np.transpose(rot_mat(θ)))
+    assert np.isclose(0, np.max(np.abs(I - I_bck)))
