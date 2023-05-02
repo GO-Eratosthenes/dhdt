5a0d16e32cc818dcde079fdd3edf7a466fb4a08e
Francesco Nattino <f.nattino@esciencecenter.nl>
2023-05-02 13:23:12 +0200

    remove custom numpy print

--- dhdt/__init__.py
+++ dhdt/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import numpy as np
 
 from .__version__ import __version__
 
@@ -9,5 +8,3 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Bas Altena"
 __email__ = 'b.altena@uu.nl'
-
-np.set_printoptions(precision=3, suppress=True)
