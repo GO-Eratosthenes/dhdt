302f9b9fe0f1e0ed7440ff78d2f23442e9718d42
Bas Altena <b.altena@uu.nl>
2023-11-23 10:09:16 +0100

    resolving PEP8

--- dhdt/testing/classification_tools.py
+++ dhdt/testing/classification_tools.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def calculate_accuracy(Predicted, Truth):
     acc = 1 - np.divide(np.sum(np.logical_xor(Predicted, Truth)),
                         Truth.size)
--- tests/preprocessing/test_shadow_detection.py
+++ tests/preprocessing/test_shadow_detection.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 
 from pathlib import Path
 from skimage.filters import threshold_otsu
@@ -7,17 +6,19 @@ from skimage.filters import threshold_otsu
 from dhdt.generic.mapping_io import read_geo_image
 from dhdt.preprocessing.image_transforms import mat_to_gray
 from dhdt.preprocessing.shadow_transforms import (apply_shadow_transform,
-    _get_shadow_transforms)
+                                                  _get_shadow_transforms)
 from dhdt.testing.classification_tools import calculate_accuracy
 
 TEST_DATA = os.path.join(Path(__file__).parents[2],
-                        'testdata/ShadowImages/shadow-5cvx')
+                         'testdata/ShadowImages/shadow-5cvx')
+
 
 def _get_shadow_test_imagery():
-    I = read_geo_image(TEST_DATA + '.jpg')[0]
-    I = mat_to_gray(I)
-    M = read_geo_image(TEST_DATA + '.png')[0]
-    return I, M
+    Img = read_geo_image(TEST_DATA + '.jpg')[0]
+    Img = mat_to_gray(Img)
+    Msk = read_geo_image(TEST_DATA + '.png')[0]
+    return Img, Msk
+
 
 # testing functions, based on imagery from:
 # https://www3.cs.stonybrook.edu/~minhhoai/projects/shadow.html
@@ -26,16 +27,17 @@ def test_shadow_transforms():
     methods = list(_get_shadow_transforms().keys())
     methods = list(methods[i] for i in [0, 5, 6, 7, 8, 10, 11, 12])
 
-    I, M = _get_shadow_test_imagery()
+    Img, Msk = _get_shadow_test_imagery()
     for method in methods:
-        S = apply_shadow_transform(method, I[...,2], I[...,1], I[...,0],
-                                   None, None, None)
+        Scn = apply_shadow_transform(method,
+                                     Img[..., 2], Img[..., 1], Img[..., 0],
+                                     None, None, None)
         if method in ['sdi', 'c3']:
-            T = S < threshold_otsu(S)
+            Class = Scn < threshold_otsu(Scn)
         else:
-            T = S > threshold_otsu(S)
+            Class = Scn > threshold_otsu(Scn)
 
         # estimate classification metrics
-        acc = calculate_accuracy(M, T!=0)
-        assert acc>.7, 'accuracy of classification seems to low'
+        acc = calculate_accuracy(Msk, Class != 0)
+        assert acc > .7, 'accuracy of classification seems to low'
     return
